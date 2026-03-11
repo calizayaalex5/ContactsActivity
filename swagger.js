aaab7f81b99037201ebc6f70426206a94655579f
calizayaalex5 <calizayaalex5@gmail.com>
2026-03-11 02:51:42 -0300

    fix swagger

--- swagger.js
+++ swagger.js
@@ -10,6 +10,6 @@ const doc = {
 };
 
 const outputFile = './swagger.json'
-const endPointsFiles = ['./routes/index.js']
+const endPointsFiles = ['./routes/index.js', './routes/users.js']
 
 swaggerAutogen(outputFile, endPointsFiles, doc)
